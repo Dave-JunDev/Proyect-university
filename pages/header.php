7dad96aa984aae7ddf7ecf0d17f9a4fcaa9cf05f
David Steven <david.steven.97.t@gmail.com>
2023-03-13 16:17:56 -0500

    imagenes

--- pages/header.php
+++ pages/header.php
@@ -1,28 +1,23 @@
 
-
-        <header>
+<header>
             <section class="header">
                 <nav class="navbar navbar-expand-lg navbar-dark">
-                    <div class="container-fluid">
-                        <a class="navbar-brand" href="./../index.php">Home</a>
+                    <div class="container-fluid" id="navbar-container">
+                        <a class="navbar-brand" href="index.php"><img src="./../img/Logo/logo_h.png" class="brand" alt="logo"></a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                           <span class="navbar-toggler-icon"></span>
                         </button>
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                           </ul>
                           <ul class="navbar-nav me-4 mb-lg-0">
                             <li>
-                                <a class="nav-link" aria-current="page" href="./Empresas.php">Login Empresas</a>
+                                <a class="nav-link" aria-current="page" href="./pages/Empresas.php">EMPRESAS</a>
                             </li>
                             <li>
-                                <a class="nav-link" aria-current="page" href="./Usuarios.php">Login Usuarios</a>
+                                <a class="nav-link" aria-current="page" href="./pages/Usuarios.php">USUARIOS</a>
                             </li>
                           </ul>
-                          <form class="d-flex">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                            <button class="btn btn-outline-success" type="submit">Search</button>
-                          </form>
                         </div>
                       </div>
                 </nav>
